17800c5413b08a827cbd32d8ab6cd9f6ee2b6a53
maxim-shlain <maxshlain@gmail.com>
2023-04-29 19:36:11 +0300

    add main app logic

--- src/multiroute.app/App.xaml.cs
+++ src/multiroute.app/App.xaml.cs
@@ -1,12 +1,49 @@
-﻿namespace multiroute.app;
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using multiroute.core;
+using Serilog;
+
+namespace multiroute.app;
 
 public partial class App : Application
 {
-	public App()
-	{
-		// TODO: Please give feedback on the inclusion of styles defined in XAML at https://github.com/mrlacey/MauiAppAccelerator/issues/10
-		InitializeComponent();
+    private readonly ICommandLineArgumentsProvider _provider;
+    private readonly IBrowserRouter _router;
 
-		MainPage = new MainPage(new MainViewModel());
-	}
+    public App(ICommandLineArgumentsProvider provider, IBrowserRouter router)
+    {
+        InitializeComponent();
+
+        _provider = provider;
+        _router = router;
+
+
+        OpenBrowserAndShowBanner();
+    }
+
+    private async void OpenBrowserAndShowBanner()
+    {
+        // Debugger.Launch();
+        string bannerText;
+
+        try
+        {
+            var args = _provider.GetCommandLineArguments();
+            var routeConfig = _router.OpenBrowser(args);
+            bannerText = $"Starting {routeConfig.Name}";
+            Log.Information("{bannerText}", bannerText);
+        }
+        catch (Exception e)
+        {
+            bannerText = e.Message;
+        }
+
+        new ToastContentBuilder()
+            .AddText("Route completed")
+            .AddText(bannerText)
+            .Show();
+
+        await Task.Delay(3000);
+
+        Current?.Quit();
+    }
 }
