7aaf1b05fc4da473bccac7c787be14d7f839f67a
maxim-shlain <maxshlain@gmail.com>
2023-04-29 19:25:38 +0300

    add tests

---
+++ src/multiroute.tests/BrowserRouterTests.cs
@@ -0,0 +1,41 @@
+﻿using multiroute.core;
+using NSubstitute;
+
+namespace multiroute.tests;
+
+public class BrowserRouterTests
+{
+    [Fact]
+    public void WillOpenBingExecutableForBingUrl()
+    {
+        var starter = Substitute.For<IProcessStarter>();
+        var router = new BrowserRouter(
+            RouterTestFixtures.Routes,
+            starter
+        );
+
+        router.OpenBrowser(new[] { "test.exe", "http://www.bing.com" });
+
+        starter.Received(1).StartProcess(
+            RouterTestFixtures.Routes.Rules["bing"].Path,
+            Arg.Is<List<string>>(x => x.Any(y => y == "http://www.bing.com"))
+        );
+    }
+
+    [Fact]
+    public void WillOpenChromeExecutableForGoogleUrl()
+    {
+        var starter = Substitute.For<IProcessStarter>();
+        var router = new BrowserRouter(
+            RouterTestFixtures.Routes,
+            starter
+        );
+
+        router.OpenBrowser(new[] { "test.exe", "http://www.google.com" });
+
+        starter.Received(1).StartProcess(
+            RouterTestFixtures.Routes.Rules["google"].Path,
+            Arg.Is<List<string>>(x => x.Any(y => y == "http://www.google.com"))
+        );
+    }
+}
---
+++ src/multiroute.tests/RouterTestFixtures.cs
@@ -0,0 +1,43 @@
+﻿using multiroute.core;
+
+namespace multiroute.tests;
+
+public static class RouterTestFixtures
+{
+    public static readonly Routes Routes = new()
+    {
+        Default = new RouteConfig
+        {
+            Name = "Edge (Default)",
+            Path = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
+            Args = ""
+        },
+        Rules = new Dictionary<string, RouteConfig>
+        {
+            {
+                "bing", new RouteConfig
+                {
+                    Name = "Edge for bing",
+                    Path = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
+                    Args = ""
+                }
+            },
+            {
+                "google", new RouteConfig
+                {
+                    Name = "Chrome for google",
+                    Path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+                    Args = ""
+                }
+            },
+            {
+                "ynet", new RouteConfig
+                {
+                    Name = "Chrome Incognito for ynet",
+                    Path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+                    Args = "--incognito"
+                }
+            }
+        }
+    };
+}
